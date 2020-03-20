e04cab9e0e4048ea9a782a3723920c4129cb4571
lzz <352840359@qq.com>
2020-03-20 20:46:27 +0800

    first commit

---
+++ public/precache-manifest.83645eb3a74335b6cbe671e9efc9345d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6d5a9748accfb4356a9efc8b1aad2dd0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1672de290a02ce84a3bd",
+    "url": "/static/css/2.f05ff9ab.chunk.css"
+  },
+  {
+    "revision": "1672de290a02ce84a3bd",
+    "url": "/static/js/2.60c596e6.chunk.js"
+  },
+  {
+    "revision": "5d5baf5b2e2033719dec534d32738d87",
+    "url": "/static/js/2.60c596e6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "75ab43701294d18bd8f3",
+    "url": "/static/js/main.427ca96a.chunk.js"
+  },
+  {
+    "revision": "d5dda6f20276eaacc67b",
+    "url": "/static/js/runtime-main.1e5ceb7d.js"
+  },
+  {
+    "revision": "639dc7b783ca01ec9701dd62a9005ca1",
+    "url": "/static/media/bg.639dc7b7.jpg"
+  }
+]);
